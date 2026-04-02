3398e8ed67d37f6f0b62adbec9a95fd39014efd0
LyudmilDS <lyudmilds@gmail.com>
2026-04-02 15:38:48 +0300

    fix white spaces and add comments

--- Main.cpp
+++ Main.cpp
@@ -1,21 +1,12 @@
-﻿//system includes
-#include <iostream>
-#include <vector>
-#include <string>
-
 //user includes
 #include"Game.hpp"
 #include"Player.hpp"
-#include"Card.hpp"
-#include"Saving_to_file.hpp"
-
-using namespace std;
 
 int main()
 {
      Player yugi_muto("Yugi Muto"), pegasus("Pegasus");
      Game game(yugi_muto, pegasus);
 
-	 game.startGame();
+	 game.startGame(yugi_muto, pegasus);
 
 }
